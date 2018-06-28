e09321ffcedd3fee04d017a8363d103af366191d
Unknown <surikhin@rambler.ru>
2018-06-29 01:39:20 +0700

    file for homework with data

---
+++ tileHomework/tileHomework.js
@@ -0,0 +1,36 @@
+
+/*
+array of objects, NO NEED to use JSON.parse here
+ */
+let data = [
+    {
+        name:"tile",
+        price: 15,
+        height: 5,
+        width: 5
+    },
+    {
+        name:"granit",
+        price: 25,
+        height: 3,
+        width: 3
+    },
+    {
+        name: "stone",
+        price: 10,
+        height: 6,
+        width: 4
+    },
+    {
+        name: "concrete",
+        price: 12,
+        height: 3,
+        width: 6
+    },
+    {
+        name: "wood",
+        price: 8,
+        height: 10,
+        width: 10
+    }
+];
