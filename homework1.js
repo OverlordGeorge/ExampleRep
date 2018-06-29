be5f0066c3ed1711ee0b626ecf230e009bc47475
Brianna <althage.brianna@gmail.com>
2018-06-28 23:07:05 -0500

    Auto stash before merge of "Brianna" and "origin/master"
    finish tileHomework commit

--- homework1.js
+++ homework1.js
@@ -14,20 +14,35 @@ function makeRequest (url, callback){
 
 // you need to write code here, because only in callback you will receive data not earlier
 function dealWithData(data) {
-    console.log(data);
-    //return data;
-    //write code here which will be creating class for every element of data via loop
-    //you might even no need to pecial functions inside class for sorting, try to make make it outside
-    //first of all deal with putting in arr of classes data
-    /*
-    let arr = [];
-    for (let i=0;i<data.length;i++)
-    {
-        create new elem of the class put info in it and then push new elem into array
-    }
-    then sort array with outside function
-     */
+  let obj= JSON.parse(data);
+
+    
+let arr=[];
+for (let i=0; i<obj.length; i++){
+  let temp= new DataClass(obj[i].userId, obj[i].id, obj[i].title, obj[i].body);
+
+  arr.push(temp);
 }
+sortByBiggestBody(arr);
+console.log(arr);
+}
+
+function sortByBiggestBody(data){
+    for (var i=0; i<=data.length; i++){
+        for (var j=0; j<data.length-1; j++){
+          if (data[j].body&&data[j+1].body){
+            if (data[j].body.length< data[j+1].body.length){
+                let temp= data[j];
+                data[j]= data[j+1];
+                data[j+1]= temp;
+            }
+          }
+        }
+    }
+
+}
+
+
 
 class DataClass {
     constructor (userId, id, title, body){
@@ -35,6 +50,7 @@ class DataClass {
         this.id= id;
         this.title=title;
         this.body= body;
+      }
 
         /*function biggestBody(data){
             let largestBodyLength=0;
@@ -63,7 +79,6 @@ class DataClass {
 
 
     }
-}
 
 
 
@@ -71,7 +86,7 @@ let ourUrl = "https://jsonplaceholder.typicode.com/posts";
 
 let dataInfo= makeRequest (ourUrl, dealWithData);
 
-console.log(dataInfo);
+//console.log(dataInfo);
 let class1= new DataClass (dataInfo);
 //console.log(DataClass.biggestBody(class1));  // you should call class1.biggestBody(dataInfo) instead DataClass.biggestBody
 // DataClass is a type of variables, like string or integer
--- tileHomework/tileHomework.js
+++ tileHomework/tileHomework.js
@@ -1,7 +1,66 @@
+class TileClass {
+  constructor (name, price, height, width){
+    this.name= name;
+    this.price= price;
+    this.height=height;
+    this.width= width;
+  }
+  areaOfTiles(){
+  return this.height * this.width;
+}
+  calcPrice(N){
+    return N * this.price;
+  }
+}
 
-/*
-array of objects, NO NEED to use JSON.parse here
- */
+function sortByArea(M,arr){
+  let areaArray= [];
+  for (let i=0; i<arr.length; i++){
+    if (arr[i].areaOfTiles() >M){
+      areaArray.push(arr[i]);
+    }
+  }
+  return areaArray;
+}
+
+
+function classArr (arr){
+  let classArray=[];
+  for (let i=0; i<arr.length; i++){
+  let temp= new TileClass(arr[i].name, arr[i].price, arr[i].height, arr[i].width);
+  classArray.push(temp);
+}
+return classArray;
+}
+
+function sortArea(arr){
+  for (let j=0; j<=arr.length; j++){
+    for (let i=0; i<arr.length-1; i++){
+      if (arr[i].areaOfTiles() > arr[i+1].areaOfTiles()){
+        let temp= arr[i];
+        arr[i]= arr[i+1];
+        arr[i+1]=temp;
+      }
+    }
+  }
+
+}
+
+function sortLess(arr){
+  for (let j=0; j<=arr.length; j++){
+    for (let i=0; i<arr.length-1; i++){
+      if (arr[i].price>arr[i+1].price){
+        let temp=arr[i];
+        arr[i]= arr[i+1];
+        arr[i+1]=temp;
+      }
+    }
+  }
+return arr;
+}
+
+
+
 let data = [
     {
         name:"tile",
@@ -34,3 +93,15 @@ let data = [
         width: 10
     }
 ];
+
+let arrayOfClasses= classArr(data);
+console.log(arrayOfClasses);
+sortArea(arrayOfClasses);
+console.log(arrayOfClasses);
+let sortedByArea= sortByArea(20, arrayOfClasses);
+//console.log(sortedByArea);
+//console.log(arrayOfClasses[2].areaOfTiles());
+//console.log(arrayOfClasses[2].calcPrice(3));
+//let class1= new TileClass(data);
+//console.log(areaOfTiles(class1));
+//console.log(sortLess(arrayOfClasses));
