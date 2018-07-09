5a0e2c42fdfd459c8aba890642d13a2d18ebe6d2
Unknown <surikhin@rambler.ru>
2018-07-09 22:42:32 +0700

    data for homework

---
+++ studentsBuild/studentsBuilding.js
@@ -0,0 +1,27 @@
+let data = [
+    {
+        name: "TestUniversity",
+        year: 1901,
+        boss: "Nick Shepard"
+    },
+    {
+        name: "Harvard",
+        year: 1636,
+        boss: "Lawrence Seldon Bacow"
+    },
+    {
+        name: "SibFU",
+        year: 2006,
+        boss: "Vladimir Kolmakov"
+    },
+    {
+        name: "Cambridge",
+        year: 1209,
+        boss: "David John Sainsbury"
+    },
+    {
+        name: "LMSU",
+        year: 1755,
+        boss: "Victor Sadovnich"
+    }
+];
