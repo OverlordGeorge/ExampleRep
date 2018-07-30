4b8e52553940d4c56e4b31d1c1b79c39fae7da65
Brianna <althage.brianna@gmail.com>
2018-07-30 14:33:32 -0500

    changes

--- javascript/addingDiv.js
+++ javascript/addingDiv.js
@@ -5,13 +5,17 @@ function changeMe(id) {
     let div = document.getElementById(id);
 
 
-    //read new text from prompt (like alert)
+
     let newText = prompt('write new text','');
     div.textContent = newText;
-    // after changing text content we lost all buttons so lets
-    // return them
+
+
     let button1 = document.createElement('button');
     button1.textContent = "change";
+    button1.style.backgroundColor= "orange";
+    button1.style.color= "white";
+      button1.style.margin="2%";
+
     button1.onclick = function(){
         let button = this;
         let div = button.parentElement;
@@ -21,6 +25,9 @@ function changeMe(id) {
 
     let button2 = document.createElement('button');
     button2.textContent = "delete";
+    button2.style.backgroundColor= "red";
+    button2.style.color= "white";
+      button2.style.margin="2%";
     button2.onclick = function(){
         let button = this;
         let div = button.parentElement;
@@ -30,6 +37,9 @@ function changeMe(id) {
 
     let button3 = document.createElement('button');
     button3.textContent = "choose";
+    button3.style.backgroundColor= "green";
+    button3.style.color= "white";
+      button3.style.margin="2%";
     button3.onclick = function(){
         let button = this;
         let div = button.parentElement;
@@ -52,13 +62,22 @@ function deleteMe(id) {
 
 function chooseMe(id) {
     let div = document.getElementById(id);
-    div.style.backgroundColor= "brown";
+    div.style.backgroundColor= "gray";
 }
 
-function chooseAll(id) {
-    let div = document.getElementById(id);
-    div.style.backgroundColor="brown";
+/*function chooseAll() {
+    let divs = document.getElementsByClassName('row');
+    divs.style.backgroundColor="gray";
 }
+
+function deleteChosen() {
+  let divs=document.getElementsByClassName('row');
+if (divs.style.backgroundColor="gray"){
+
+      let body = document.body;
+      body.removeChild(divs);
+}
+*/
 
 
 
@@ -68,12 +87,16 @@ function add() {
     let body = document.body;
     let div = document.createElement('div');
     div.className = "row";
+    div.style.padding="2%";
     div.style.border= "solid";
     div.id = id;
     div.textContent = document.getElementById('input').value;
 
     let button1 = document.createElement('button');
     button1.textContent = "change";
+    button1.style.backgroundColor= "orange";
+    button1.style.color= "white";
+    button1.style.margin="2%";
     button1.onclick = function(){
         let button = this;
         let div = button.parentElement;
@@ -83,6 +106,9 @@ function add() {
 
     let button2 = document.createElement('button');
     button2.textContent = "delete";
+    button2.style.backgroundColor= "red";
+    button2.style.color= "white";
+      button2.style.margin="2%";
     button2.onclick = function(){
         let button = this;
         let div = button.parentElement;
@@ -92,6 +118,9 @@ function add() {
 
     let button3 = document.createElement('button');
     button3.textContent = "choose";
+    button3.style.backgroundColor= "green";
+    button3.style.color= "white";
+      button3.style.margin="2%";
     button3.onclick = function(){
         let button = this;
         let div = button.parentElement;
