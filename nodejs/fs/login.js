c00db247758cc99d4da2a9ad493d0170f9e53280
Brianna <althage.brianna@gmail.com>
2018-10-13 15:06:52 -0500

    changes

--- nodejs/fs/login.js
+++ nodejs/fs/login.js
@@ -15,7 +15,21 @@ function sendLogin(){
     });
 }
 
-function register() {
+function register(){
+    let name = $("#name").val();
+    let login = $("#login").val();
+    let password= $("#pass").val();
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/regApi",
+        data: {
+            'name': name,
+            'login': login,
+            'password': password,
 
-
+        },
+        success: function (data){
+            alert(data);
+        }
+    });
 }
