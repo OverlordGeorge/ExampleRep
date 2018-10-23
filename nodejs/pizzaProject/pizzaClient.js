a138f6184516bf65d3aca3a62b1318362e7a1daf
Brianna <althage.brianna@gmail.com>
2018-10-23 01:10:05 -0500

    changes

--- nodejs/pizzaProject/pizzaClient.js
+++ nodejs/pizzaProject/pizzaClient.js
@@ -39,25 +39,36 @@ function showPizza(data){
     listDiv.empty();
     console.log(data);
     for (let i=0; i<data.length; i++){
-        let rowDiv= document.createElement("p");
+        let rowDiv= document.createElement("div");
         rowDiv.style.border="1px solid";
         rowDiv.style.padding="1%";
+        rowDiv.style.width="30%";
+
+
+        let nameDiv= document.createElement("div");
         let name= data[i].name;
+        nameDiv.append(name);
+        nameDiv.style.width="100px";
+        nameDiv.style.display="inline-block";
+        let priceDiv= document.createElement("div");
         let price= data[i].price;
+        priceDiv.append(price);
+        priceDiv.style.width="100px";
+        priceDiv.style.display="inline-block";
         let editBtn= document.createElement("button");
         editBtn.id=data[i]._id;
-        editBtn.style.margin="2%";
+        editBtn.style.margin="1%";
         editBtn.append("edit");
         editBtn.onclick= function() { update(data[i]._id); };
         editBtn.style.backgroundColor="purple";
         let deleteBtn= document.createElement("button");
         deleteBtn.id= data[i]._id;
-        deleteBtn.style.margin="2%";
+        deleteBtn.style.margin="1%";
         deleteBtn.append("delete");
         deleteBtn.onclick= function() { deletePizza(data[i]._id); };
         deleteBtn.style.backgroundColor="red";
-        rowDiv.append(name + " ");
-        rowDiv.append(price+ " ");
+        rowDiv.append(nameDiv);
+        rowDiv.append(priceDiv);
         rowDiv.append(editBtn);
         rowDiv.append(deleteBtn);
 listDiv.append(rowDiv);
