4d6ac468f289e3cc90c770f36b83cdad8ff9d568
Unknown <surikhin@rambler.ru>
2019-05-12 16:57:15 +0700

    some data for hwk

---
+++ angular1/standartDirectivesHmw/script.js
@@ -0,0 +1,24 @@
+let data = {
+    jamaicaBtn: [
+        {
+            name: "Negril",
+            image: "images/jamaica1.jpg"
+        },
+        {
+            name: "Ocho rios",
+            image: "images/jamaica2.jpg"
+        }
+    ],
+    mexicoBtn: [
+        {
+            name: "Reviera",
+            image: "images/mexico1.jpg"
+        }, {
+            name: "Cabo San Lucas",
+            cityImage: "images/mexico2.jpg"
+        }, {
+            name: "Costa and Playa Mujeres",
+            cityImage: "images/mexico3.jpg"
+        }
+    ]
+};
