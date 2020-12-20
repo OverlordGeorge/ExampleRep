c81eddf1f1b71aaf8f1db6ddd6f94f62e7eded4f
Brianna <althage.brianna@gmail.com>
2020-12-20 00:25:56 -0600

    changes

--- Angular2/angularBasics/app/src/app/app.component.ts
+++ Angular2/angularBasics/app/src/app/app.component.ts
@@ -8,8 +8,34 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   show = true;
+  name;
+  pizzas = [
+      {name: "cheese",
+          price: 15,
+          likes: 43
+      },
+      {name: "meat",
+          price: 25,
+          likes: 53
+      },
+      {name: "veggie",
+          price: 10,
+          likes: 20
+      }
+  ];
+  cart=[];
+sum=0;
 
   changeShow(){
     this.show= !this.show
   }
+ addLike(pizza){
+    pizza.likes++
+ }
+
+ addToCart(pizza){
+    this.cart.push(pizza);
+    this.sum+= pizza.price
+ }
+
 }
