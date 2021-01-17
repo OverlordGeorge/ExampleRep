16429c2194843a8241a0070bdf95da81abe7e99a
Brianna <althage.brianna@gmail.com>
2021-01-16 22:18:25 -0600

    changes

--- Angular2/angularBasics/my-app/src/app/app.component.ts
+++ Angular2/angularBasics/my-app/src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+export interface Profile {
+  name: string,
+    username: string
+
+}
+
+export interface Blog {
+    author: string,
+    text: string
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +19,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+profileInfo: Profile= {
+  name: "Brianna",
+    username: "Bree09"
+}
+blogs: Blog[]= [
+    {author: "healthBlog",
+    text: "Food is Medicine"},
+    {author: "politicPeople",
+    text:"The new president is coming"}
+]
 }
--- Angular2/angularBasics/my-app/src/app/app.module.ts
+++ Angular2/angularBasics/my-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule
